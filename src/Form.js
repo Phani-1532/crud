8b3a79b0b2ffabe610fcc0d75d60b88e470b30b1
Phanindra <phanindragadireddy2@gmail.com>
2025-02-06 09:15:33 +0530

    conditional errors addes

--- src/Form.js
+++ src/Form.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 
 function Form({ interChange, initializeFormData, postProducts }) {
   const [products, setProducts] = useState(initializeFormData)
+  const [submitted, setSubmitted] = useState(false)
 
   //this function give access to write in input fields
   function changeFormData(e) {
@@ -14,10 +15,12 @@ function Form({ interChange, initializeFormData, postProducts }) {
         <div className='form-group'>
           <label htmlFor='name'>Name:</label>
           <input type='text' name='name' value={products.name} onChange={changeFormData} className='form-control mt-2' id='name' aria-describedby='nameHelp' placeholder='Enter mobile name' />
+          {submitted && products.name === '' && <span className='text-danger'>Name is required</span>}
         </div>
         <div className='form-group'>
           <label htmlFor='price'>Price:</label>
           <input type='number' value={products.price} onChange={changeFormData} name='price' className='form-control mt-2' id='price' aria-describedby='priceHelp' placeholder='Enter mobile price' />
+          {submitted && products.price === '' && <span className='text-danger'>Price is required</span>}
         </div>
         <div className='form-group'>
           <label htmlFor='category'>Category:</label>
@@ -28,13 +31,20 @@ function Form({ interChange, initializeFormData, postProducts }) {
             <option value="tv's">Tv's</option>
             <option value='wifi'>Wifi</option>
           </select>
+          {submitted && products.category === '' && <span className='text-danger'>Category is required</span>}
         </div>
 
 
         <button className='btn btn-primary float-end' onClick={(e) => {
+          setSubmitted(true)
           e.preventDefault()
-          postProducts(products)
-        }}>Send</button>
+          if(!!products.name && !!products.price && !!products.category){
+            postProducts(products)
+            
+          }
+        }}
+        >Send</button>
+
 
         <button className='btn btn-danger float-end' onClick={(e) => {
           e.preventDefault()
